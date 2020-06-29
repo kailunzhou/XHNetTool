925a6ac450720e4c2bc65854cbc7886faf19f786
zklcode <372909335@qq.com>
2020-06-29 10:03:34 +0800

    添加无token类型的通用接口方法

--- Classes/BaseNet.swift
+++ Classes/BaseNet.swift
@@ -9,15 +9,60 @@ open class BaseNet: NSObject {
     public var token: String?//token值
     public var service: String? //服务器地址
     /*
+     用于：只需要传入data内参数的接口，而且都需要token
      urlStr: 接口地址拼接部分
-     data: 传入参数
+     data: 传入data参数
      view: 当前的视图
      isParse: 返回的数据是否要先解析
      endAction: 请求完成就会调用的事件
      okAction: 解析完成调用的事件
      */
-    public func BaseNetRequest(_ urlStr: String, _ data: [String: Any]?, _ view: UIView?, _ isParse: Bool, endAction: @escaping ()->(), okAction: @escaping (_ result: Any?)->()) {
-        netRequest(urlStr: urlStr, method: .post, data: data, beginDeal: {
+    public func BaseNetDataRequest(_ urlStr: String, _ data: [String: Any]?, _ view: UIView?, _ isParse: Bool, endAction: @escaping ()->(), okAction: @escaping (_ result: Any?)->()) {
+        guard let tokens = token else {return}
+        var param: [String: Any] = ["token": tokens]
+        if let datas = data {
+            param["data"] = datas
+        }
+        netRequest(urlStr: urlStr, method: .post, param: param, beginDeal: {
+            MBProgressHUD.showLoading(view, with: nil)
+        }, endDeal: {
+            MBProgressHUD.dismisHUD(view)
+        }, success: { (result) in
+            endAction()
+            if isParse {
+                if let code = result["code"] as? Int {
+                    switch code {
+                    case 0:
+                        if let data = result["data"] {
+                            okAction(data)
+                        } else {
+                            okAction(nil)
+                        }
+                    case -100:
+                        self.tokenInvalid?()
+                    default:
+                        if let msg = result["msg"] as? String {
+                            MBProgressHUD.showMessage(view, with: msg, complete: nil)
+                        }
+                    }
+                }
+            } else {
+                okAction(result)
+            }
+        }) { (err) in
+        }
+    }
+    /*
+    用于：1.不需要token传入 或者 2.有与token平级的其它参数传入 的接口
+    urlStr: 接口地址拼接部分
+    param: 传入参数
+    view: 当前的视图
+    isParse: 返回的数据是否要先解析
+    endAction: 请求完成就会调用的事件
+    okAction: 解析完成调用的事件
+    */
+    public func BaseNetParamRequest(_ urlStr: String, _ param: [String: Any]?, _ view: UIView?, _ isParse: Bool, endAction: @escaping ()->(), okAction: @escaping (_ result: Any?)->()) {
+        netRequest(urlStr: urlStr, method: .post, param: param, beginDeal: {
             MBProgressHUD.showLoading(view, with: nil)
         }, endDeal: {
             MBProgressHUD.dismisHUD(view)
@@ -80,20 +125,23 @@ open class BaseNet: NSObject {
      可以本地自己对这个方法进行封装处理。
      */
     ///一般接口处理
-    public func netRequest(urlStr: String, method: HTTPMethod, data: [String : Any]?, beginDeal: @escaping ()->(), endDeal: @escaping ()->(), success: @escaping (_ response : [String : Any])->(), failture: @escaping (_ error : Error?)->()) {
+    public func netRequest(urlStr: String, method: HTTPMethod, param: [String : Any]?, beginDeal: @escaping ()->(), endDeal: @escaping ()->(), success: @escaping (_ response : [String : Any])->(), failture: @escaping (_ error : Error?)->()) {
         guard let tokens = token, let services = service else {return}
         if proxyStatus() {return}
         let header: HTTPHeaders = [
             "Authorization": "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==",
             "Accept": "application/json"
         ]
-        var param: [String: Any] = ["userId": "11111111111", "sign": "AGFSDFRTGF56GTS",
-                                    "timeStamp": getTimeStamp(), "token": tokens]
-        if let datas = data {
-            param["data"] = datas
+        var paramDic: [String: Any] = [:]
+        if let params = param {
+            paramDic = params
         }
+        paramDic["userId"] = "11111111111"
+        paramDic["sign"] = "AGFSDFRTGF56GTS"
+        paramDic["version"] = "1.1"
+        paramDic["timeStamp"] = getTimeStamp()
         beginDeal()
-        Alamofire.request(services + urlStr, method: method, parameters: param, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+        Alamofire.request(services + urlStr, method: method, parameters: paramDic, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
             endDeal()
             switch response.result {
             case .success:
@@ -103,7 +151,7 @@ open class BaseNet: NSObject {
                 }
                 let jsonStr = successDic.showJsonString
                 #if DEBUG
-                print("\n请求链接:\(urlStr)\n请求参数:\(param)\n请求结果:\(jsonStr)")
+                print("\n请求链接:\(urlStr)\n请求参数:\(paramDic)\n请求结果:\(jsonStr)")
                 #endif
                 success(successDic)
             case .failure(let error):
